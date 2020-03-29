ee29cd179c1fb3090f4fb66222ad5446643cdeaa
Maksim Ivanov <satansdeer@gmail.com>
2020-03-29 15:29:40 +0200

    Persist logged in state

--- src/Auth.js
+++ src/Auth.js
@@ -5,10 +5,18 @@ export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
 
   useEffect(() => {
-    app.auth().onAuthStateChanged(setCurrentUser);
+    app.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user)
+      setPending(false)
+    });
   }, []);
+
+  if(pending){
+    return <>Loading...</>
+  }
 
   return (
     <AuthContext.Provider
