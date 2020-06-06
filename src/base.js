0a11c740a0dccf22ae711f084caf02bcade71502
Junip Dewan <52928789+junip-dewan@users.noreply.github.com>
2020-06-06 22:59:32 +0530

    :arrow_up: firebase version updated to latest and updated env keys & auth configs

--- src/base.js
+++ src/base.js
@@ -7,7 +7,9 @@ const app = firebase.initializeApp({
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 export default app;
