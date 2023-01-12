127ff7f510c79d3109a28d4da53c2e13c93ff9a9
farhanatfunkarah <funkarahdotpk@gmail.com>
2023-01-12 22:46:58 +0500

    ok

--- src/base.js
+++ src/base.js
@@ -9,7 +9,6 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 export default app;
